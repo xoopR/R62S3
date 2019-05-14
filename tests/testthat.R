b5bbd0c31392da718d4b0ef3f22ebb4889f94703
Raphael <raphael.sonabend.15@ucl.ac.uk>
2019-05-14 10:08:15 +0100

    Added tests

---
+++ tests/testthat.R
@@ -0,0 +1,5 @@
+library(testthat)
+library(R62S3)
+
+test_check("R62S3")
+
---
+++ tests/testthat/test-R62S3.R
@@ -0,0 +1,20 @@
+library(testthat)
+
+context("R62S3")
+
+nogen <- R6::R6Class("nogen",public = list(lbind = function() return("Test No Gen")))
+test_that("no generic",{
+  expect_silent(R62S3(nogen))
+  expect_equal(lbind(nogen$new()), "Test No Gen")
+  expect_silent(get("lbind"))
+  expect_silent(get("lbind.nogen"))
+})
+
+gen <- R6::R6Class("gen",public = list(rbind = function() return("Test Gen")))
+
+test_that("generic",{
+  expect_silent(R62S3(gen))
+  expect_equal(rbind(gen$new()), "Test Gen")
+  expect_silent(get("rbind.gen"))
+  expect_silent(get("rbind"))
+})
