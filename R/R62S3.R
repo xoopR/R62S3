23244b75fb5f5ab1f55e88271b144e2ee2433ff2
Raphael <raphael.sonabend.15@ucl.ac.uk>
2019-05-16 16:02:54 +0100

    Updates for CRAN
    - Removed assignment to GlobalEnv

--- R/R62S3.R
+++ R/R62S3.R
@@ -4,7 +4,7 @@
 #' @param R6Class R6ClassGenerator to generate public methods from
 #' @param dispatchClasses list of classes to assign S3 dispatch methods on
 #' @param assignEnvir environment in which to assign the S3 generics/methods, default Global Environment
-#' @usage R62S3(R6Class, dispatchClasses = list(R6Class), assignEnvir = .GlobalEnv)
+#' @usage R62S3(R6Class, dispatchClasses = list(R6Class), assignEnvir = parent.env(environment()))
 #' @details Searches in a given R6 class for all public methods that are not 'initialize' or 'clone'.
 #' For each method if a generic does not already exist, one is created and assigned to the given environment.
 #' Methods are created for every generic, following standard S3 convention.
@@ -14,12 +14,12 @@
 #'public = list(initialize = function() {},
 #'printer = function(str) {print(str)}))
 #' pm <- printMachine$new()
-#' R62S3(printMachine)
+#' R62S3(printMachine, assignEnvir = .GlobalEnv)
 #' pm$printer("Test String A")
 #' printer(pm, "Test String B")
 #'
 #' @export
-R62S3 <- function(R6Class, dispatchClasses = list(R6Class), assignEnvir = .GlobalEnv){
+R62S3 <- function(R6Class, dispatchClasses = list(R6Class), assignEnvir = parent.env(environment())){
   checkmate::assert(inherits(R6Class,"R6ClassGenerator"),
          .var.name = "R6Class must be an R6ClassGenerator")
 
--- tests/testthat/test-R62S3.R
+++ tests/testthat/test-R62S3.R
@@ -4,7 +4,7 @@ context("R62S3")
 
 nogen <- R6::R6Class("nogen",public = list(lbind = function() return("Test No Gen")))
 test_that("no generic",{
-  expect_silent(R62S3(nogen))
+  expect_silent(R62S3(nogen, assignEnvir = .GlobalEnv))
   expect_equal(lbind(nogen$new()), "Test No Gen")
   expect_silent(get("lbind"))
   expect_silent(get("lbind.nogen"))
@@ -13,7 +13,7 @@ test_that("no generic",{
 gen <- R6::R6Class("gen",public = list(rbind = function() return("Test Gen")))
 
 test_that("generic",{
-  expect_silent(R62S3(gen))
+  expect_silent(R62S3(gen, assignEnvir = .GlobalEnv))
   expect_equal(rbind(gen$new()), "Test Gen")
   expect_silent(get("rbind.gen"))
   expect_silent(get("rbind"))
