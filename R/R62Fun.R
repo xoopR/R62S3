1cf3afc11dc5f19fff6ac14e34820eb93577242f
Raphael <raphael.sonabend.15@ucl.ac.uk>
2019-06-07 10:38:47 +0100

    Added mask

--- R/R62Fun.R
+++ R/R62Fun.R
@@ -4,9 +4,12 @@
 #' @param R6Class R6ClassGenerator to generate public methods from
 #' @param assignEnvir environment in which to assign the S4 generics/methods, default is parent of current environment.
 #' @param detectGeneric logical, if TRUE (default) detects if the method has a S3 generic and defines function accordingly
+#' @param mask logical, should the new method mask existing ones? See details.
 #' @details Searches in a given R6 class for all public methods that are not 'initialize' or 'clone'.
-#' For each method if a generic does not already exist, one is created and assigned to the given environment.
-#' Methods are created for every generic, following standard S4 convention.
+#' Creates a function to call eac of the R6 methods. Additional parameters allow the user to specify
+#' if a generic function should be detected, if so then a dispatch method is created instead. Can also
+#' choose if masking should occur, if TRUE then the function is created even if one of the same name
+#' exists, if FALSE then the function name is appended with ".classname" like a dispatch method.
 #' @return Assigns methods and generics to the chosen environment.
 #' @examples
 #' printMachine <- R6::R6Class("printMachine",
@@ -19,7 +22,7 @@
 #'
 #' @export
 R62Fun <- function(R6Class, assignEnvir = parent.env(environment()),
-                   detectGeneric = TRUE){
+                   detectGeneric = TRUE, mask = FALSE){
 
   checkmate::assert(inherits(R6Class,"R6ClassGenerator"),
                     .var.name = "R6Class must be an R6ClassGenerator")
@@ -31,12 +34,20 @@ R62Fun <- function(R6Class, assignEnvir = parent.env(environment()),
     for(i in 1:length(methods)){
       if(detectGeneric){
         generic = FALSE
-        x = tryCatch(methods(methodname),warning = function(w) w, error = function(e) e)
-        if(inherits(x, "condition")){
-          if(!grepl("appears not to be S3 generic",x$message) & !inherits(x, "error"))
+        if(mask){
+          x = tryCatch(methods(methodname),warning = function(w) w, error = function(e) e)
+          if(inherits(x, "condition")){
+            if(!grepl("appears not to be S3 generic",x$message) & !inherits(x, "error"))
+              generic = TRUE
+          } else if(length(x)!=0)
             generic = TRUE
-        } else if(length(x)!=0)
-          generic = TRUE
+        } else{
+          x = suppressWarnings(suppressMessages((try(methods(methodname),silent=T))))
+          if(class(x)!="try-error"){
+            if(length(x) > 0)
+              generic = TRUE
+          }
+        }
 
         if(generic)
           methodname = paste(names(methods)[[i]],R6Class$classname,sep=".")
