6599b14a0fca92d67e8baf1fded9b2d52080a6af
Raphael <raphael.sonabend.15@ucl.ac.uk>
2020-01-27 14:09:27 +0000

    updated tests

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -1,17 +1,24 @@
 lower = function(object, ...) UseMethod(lower, object)
 methods::setGeneric("upper", function(object, ...){}, where = .GlobalEnv)
 
-S3Generic = R6::R6Class("S3Generic",
-                        public = list(lower = function(x) tolower(x),
-                                      excluder = function() return("Excluded")),
-                        active = list(Status = function() return("Printing")))
+for (i in c("R62S3", "R62S4", "R62Fun")) {
+  assign(paste(i, "S3Generic", sep = "_"),
+         R6::R6Class(paste(i, "S3Generic", sep = "_"),
+                     public = list(lower = function(x) tolower(x),
+                                   excluder = function() return("Excluded")),
+                     active = list(Status = function() return("Printing"))))
 
-S4Generic = R6::R6Class("S4Generic",
-                        public = list(upper = function(x) toupper(x),
-                                      excluder = function() return("Excluded")),
-                        active = list(Status = function() return("Printing")))
+  assign(paste(i, "S4Generic", sep = "_"),
+         R6::R6Class(paste(i, "S4Generic", sep = "_"),
+                     public = list(upper = function(x) toupper(x),
+                                   excluder = function() return("Excluded")),
+                     active = list(Status = function() return("Printing"))))
 
-NoGeneric = R6::R6Class("NoGeneric",
-                        public = list(printer = function(x) x,
-                                      excluder = function() return("Excluded")),
-                        active = list(Status = function() return("Printing")))
+  assign(paste(i, "NoGeneric", sep = "_"),
+         R6::R6Class(paste(i, "NoGeneric", sep = "_"),
+                     public = list(printer = function(x) x,
+                                   excluder = function() return("Excluded")),
+                     active = list(Status = function() return("Printing"))))
+}
+
+rm(i)
