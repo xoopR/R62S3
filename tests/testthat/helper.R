de796b10eddb883e154c8edc5585adc06b8516a5
Raphael <raphael.sonabend.15@ucl.ac.uk>
2020-01-27 14:21:41 +0000

    updated tests

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -3,22 +3,25 @@ methods::setGeneric("upper", function(object, ...){}, where = .GlobalEnv)
 
 for (i in c("R62S3", "R62S4", "R62Fun")) {
   assign(paste(i, "S3Generic", sep = "_"),
-         R6::R6Class(paste(i, "S3Generic", sep = "_"),
-                     public = list(lower = function(x) tolower(x),
-                                   excluder = function() return("Excluded")),
-                     active = list(Status = function() return("Printing"))))
+       R6::R6Class(paste(i, "S3Generic", sep = "_"),
+                   public = list(excluder = function() return("Excluded"))))
+
+  get(paste(i, "S3Generic", sep = "_"))[["set"]]("public", paste0(i, "lower"), function(x) tolower(x))
+  get(paste(i, "S3Generic", sep = "_"))[["set"]]("active", paste0(i, "Status"), function() return("Printing"))
 
   assign(paste(i, "S4Generic", sep = "_"),
          R6::R6Class(paste(i, "S4Generic", sep = "_"),
-                     public = list(upper = function(x) toupper(x),
-                                   excluder = function() return("Excluded")),
-                     active = list(Status = function() return("Printing"))))
+                     public = list(excluder = function() return("Excluded"))))
+
+  get(paste(i, "S4Generic", sep = "_"))[["set"]]("public", paste0(i, "upper"), function(x) toupper(x))
+  get(paste(i, "S4Generic", sep = "_"))[["set"]]("active", paste0(i, "Status"), function() return("Printing"))
 
   assign(paste(i, "NoGeneric", sep = "_"),
          R6::R6Class(paste(i, "NoGeneric", sep = "_"),
-                     public = list(printer = function(x) x,
-                                   excluder = function() return("Excluded")),
-                     active = list(Status = function() return("Printing"))))
+                     public = list(excluder = function() return("Excluded"))))
+
+  get(paste(i, "NoGeneric", sep = "_"))[["set"]]("public", paste0(i, "printer"), function(x) x)
+  get(paste(i, "NoGeneric", sep = "_"))[["set"]]("active", paste0(i, "Status"), function() return("Printing"))
 }
 
 rm(i)
--- tests/testthat/test-R62S3.R
+++ tests/testthat/test-R62S3.R
@@ -4,14 +4,14 @@ context("R62S3")
 
 test_that("no generic",{
   expect_silent(R62S3(R62S3_NoGeneric, assignEnvir = topenv(), exclude = "excluder", scope = c("public","active")))
-  expect_equal(printer(R62S3_NoGeneric$new(), "Hello World"), "Hello World")
+  expect_equal(R62S3printer(R62S3_NoGeneric$new(), "Hello World"), "Hello World")
   expect_error(excluder(R62S3_NoGeneric$new()))
-  expect_equal(Status(R62S3_NoGeneric$new()), "Printing")
+  expect_equal(R62S3Status(R62S3_NoGeneric$new()), "Printing")
 })
 
 test_that("S3 generic",{
   expect_silent(R62S3(R62S3_S3Generic, assignEnvir = topenv(), exclude = "excluder", scope = c("public","active")))
-  expect_equal(lower(R62S3_S3Generic$new(), "HELLO WORLD"), "hello world")
+  expect_equal(R62S3lower(R62S3_S3Generic$new(), "HELLO WORLD"), "hello world")
   expect_error(excluder(R62S3_S3Generic$new()))
-  expect_equal(Status(R62S3_S3Generic$new()), "Printing")
+  expect_equal(R62S3Status(R62S3_S3Generic$new()), "Printing")
 })
