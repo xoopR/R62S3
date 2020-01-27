5b2ef668e441079053ebcc25551792a0327384bd
Raphael <raphael.sonabend.15@ucl.ac.uk>
2020-01-27 16:35:52 +0000

    Update detectGeneric.R

--- R/detectGeneric.R
+++ R/detectGeneric.R
@@ -19,43 +19,6 @@
       }
     }
 
-    # return(list(generic = FALSE, arg1 = arg1))
-    #
-    # if("S3" %in% type) {
-    #   if(mask){
-    #     x = tryCatch(methods(methodname),warning = function(w) w, error = function(e) e)
-    #     if(inherits(x, "condition")){
-    #       if(!grepl("appears not to be S3 generic",x$message) & !inherits(x, "error"))
-    #         return(list(generic = TRUE, arg1 = names(formals(get(methodname))[1])))
-    #     } else if(length(x)!=0)
-    #       return(list(generic = TRUE, arg1 = names(formals(get(methodname))[1])))
-    #   } else{
-    #     x = suppressWarnings(suppressMessages((try(methods(methodname),silent=T))))
-    #     y = suppressWarnings(suppressMessages((try(get(methodname),silent=T))))
-    #     if(class(x)!="try-error" | class(y)!="try-error"){
-    #       if(length(x) > 0 | length(y) > 0)
-    #         return(list(generic = TRUE, arg1 = names(formals(get(methodname))[1])))
-    #     }
-    #   }
-    # }
-    #
-    # if("S4" %in% type) {
-    #   if(mask){
-    #     x = tryCatch(methods(methodname),warning = function(w) w, error = function(e) e)
-    #     if(inherits(x, "condition")){
-    #       if(!grepl("appears not to be S3 generic",x$message) & !inherits(x, "error"))
-    #         return(list(generic = TRUE, arg1 = names(formals(get(methodname))[1])))
-    #     } else
-    #       return(list(generic = TRUE, arg1 = names(formals(get(methodname))[1])))
-    #   } else{
-    #     x = suppressWarnings(suppressMessages((try(methods(methodname),silent=T))))
-    #     if(class(x)!="try-error"){
-    #       if(length(x) > 0)
-    #         return(list(generic = TRUE, arg1 = names(formals(get(methodname))[1])))
-    #     }
-    #   }
-    # }
-    #
     return(list(generic = FALSE, arg1 = arg1))
   })
 
